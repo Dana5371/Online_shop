7b868b42eff727984c5601b471b00be22869a71a
Dana5371 <danaker.qw@gmail.com>
2022-06-07 00:50:35 +0600

    verbose name for understanding

--- main/models.py
+++ main/models.py
@@ -38,8 +38,8 @@ class News(models.Model):
 
 #Публичная оферта
 class Oferro(models.Model):
-    title = models.CharField(max_length=55)
-    description = RichTextField()
+    title = models.CharField(max_length=55, verbose_name='Заголовок')
+    description = RichTextField(verbose_name='Описание')
 
     def __str__(self):
         return self.title
@@ -50,22 +50,22 @@ class ImageHelp(models.Model):
 
 
 class Help(models.Model):
-    question = models.TextField()
-    answer = models.TextField()
+    question = models.TextField(verbose_name='Вопрос')
+    answer = models.TextField(verbose_name='Ответ')
 
 
 #Коллекция
 class Collection(models.Model):
-    image = models.ImageField()
-    title = models.CharField(max_length=55)
+    image = models.ImageField(verbose_name='Фото')
+    title = models.CharField(max_length=55,verbose_name='Название')
 
     def __str__(self):
         return self.title
 
 #Слайдер
 class Slider(models.Model):
-    image = models.ImageField(upload_to='slider_image', null=True)
-    field_link = models.CharField(max_length=150, blank=True)
+    image = models.ImageField(upload_to='slider_image', null=True, verbose_name='Фото')
+    field_link = models.CharField(max_length=150, blank=True, verbose_name='Ссылка')
 
     def __str__(self):
          return 'Some image or link'
@@ -76,10 +76,10 @@ class BackCall(models.Model):
         ('yes', 'Да'),
         ('no', 'Нет'),
     ]
-    name = models.CharField(max_length=155)
+    name = models.CharField(max_length=155, verbose_name='Имя')
     number_regex = RegexValidator(regex=r"^\+?1?\d{8,15}$")
-    number_of_phone = models.CharField(validators=[number_regex], max_length=14, unique=True, null=False, blank=False)
-    date_of_call = models.DateTimeField()
+    number_of_phone = models.CharField(validators=[number_regex], max_length=14, unique=True, null=False, blank=False,verbose_name='Номер телефона')
+    date_of_call = models.DateTimeField(verbose_name='Дата')
     status = models.CharField(choices=STATUS, default='no', max_length=155)
 
     def __str__(self):
@@ -87,21 +87,21 @@ class BackCall(models.Model):
 
 #Товар
 class Product(models.Model):
-    collection = models.ForeignKey(Collection, on_delete=models.CASCADE, related_name='product')
-    title = models.CharField(max_length=200)
-    article = models.CharField(max_length=150)
-    old_price = models.IntegerField()
-    discount = models.DecimalField(max_digits=10, decimal_places=2, default=0, blank=True)
-    new_price = models.IntegerField(blank=True)
-    description = RichTextField()
-    size = models.CharField(max_length=55, default='42-50')
-    line_of_size = models.CharField(max_length=55,blank=True, null=True)
-    compound = models.CharField(max_length=155)
-    amount = models.IntegerField()
-    material = models.CharField(max_length=150)
-    new = models.BooleanField(default=False, blank=True, null=True)
-    hit = models.BooleanField(default=False, blank=True, null=True)
-    favorite = models.BooleanField(default=False, blank=True, null=True)
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE, related_name='product',verbose_name='Коллекция')
+    title = models.CharField(max_length=200,verbose_name='Заголовок')
+    article = models.CharField(max_length=150,verbose_name='Артикль')
+    old_price = models.IntegerField(verbose_name='Старая цена')
+    discount = models.DecimalField(max_digits=10, decimal_places=2, default=0, blank=True, verbose_name='Скидка')
+    new_price = models.IntegerField(blank=True,verbose_name='Новая цена')
+    description = RichTextField(verbose_name='Описание')
+    size = models.CharField(max_length=55, default='42-50',verbose_name='Размер')
+    line_of_size = models.CharField(max_length=55,blank=True, null=True,verbose_name='Линейка')
+    compound = models.CharField(max_length=155, verbose_name='Состав')
+    amount = models.IntegerField(verbose_name='Кол-во')
+    material = models.CharField(max_length=150, verbose_name='Материал')
+    new = models.BooleanField(default=False, blank=True, null=True, verbose_name='Новинка')
+    hit = models.BooleanField(default=False, blank=True, null=True, verbose_name='Хит продаж')
+    favorite = models.BooleanField(default=False, blank=True, null=True, verbose_name='Избранные')
 
     def save(self):
         if self.discount != 0:
@@ -118,9 +118,9 @@ class Product(models.Model):
 
 
 class ProductImageColor(models.Model):
-    image = models.ImageField(upload_to='product', blank=True, null=True)
+    image = models.ImageField(upload_to='product', blank=True, null=True, verbose_name='Фото')
     color = RGBColorField()
-    products = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    products = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images', verbose_name='Товары')
 
 
 class User(models.Model):
@@ -129,15 +129,16 @@ class User(models.Model):
         ('issued', 'Оформлен'),
         ('cancelled', 'Отменен'),
     ]
-    name = models.CharField(max_length=155)
-    last_name = models.CharField(max_length=155)
-    email = models.EmailField()
+    name = models.CharField(max_length=155, verbose_name='Имя')
+    last_name = models.CharField(max_length=155, verbose_name='Фамилия')
+    email = models.EmailField(verbose_name='Почта')
     number_regex = RegexValidator(regex=r"^\+?1?\d{8,15}$")
-    number_of_phone = models.CharField(validators=[number_regex], max_length=14, unique=True, null=False, blank=False)
-    country = models.CharField(max_length=200)
-    city = models.CharField(max_length=155)
-    date_of_order = models.DateTimeField(auto_now=True)
-    status_of_order = models.CharField(choices=STATUS, default='new', max_length=155)
+    number_of_phone = models.CharField(validators=[number_regex], max_length=14, unique=True, null=False,
+                                       blank=False, verbose_name='Номер телефона')
+    country = models.CharField(max_length=200, verbose_name='Страна')
+    city = models.CharField(max_length=155, verbose_name='Город')
+    date_of_order = models.DateTimeField(auto_now=True, verbose_name='Дата')
+    status_of_order = models.CharField(choices=STATUS, default='new', max_length=155, verbose_name='Статус заказа')
 
     def __str__(self):
         return self.email
